695bd192431ce587b2c29a3448ca1e300f286d31
Atul Kabadi <atulkkabadi@gmail.com>
2024-04-21 10:15:00 +0530

    Started auth

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express, { Express } from 'express';
-import { ChattyServer } from './setupServer';
-import databaseConnection from './setupDatabase';
-import { config } from './config';
+import { ChattyServer } from '@root/setupServer';
+import databaseConnection from '@root/setupDatabase';
+import { config } from '@root/config';
+
 class Application {
   public initialize(): void {
     this.loadConfig();
@@ -12,6 +13,7 @@ class Application {
   }
   private loadConfig(): void {
     config.validateConfig();
+    config.cloudinaryConfig();
   }
 }
 const application: Application = new Application();
